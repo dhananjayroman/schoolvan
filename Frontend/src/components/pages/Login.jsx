66c1cfc971a41b66b86be43ca17efa0fd3a424cf
Dhananjay Roman <romandhananjay01@gmail.com>
2025-06-20 20:00:21 +0530

    Update Login.jsx

--- Frontend/src/components/pages/Login.jsx
+++ Frontend/src/components/pages/Login.jsx
@@ -1,10 +1,9 @@
 /* eslint-disable no-unused-vars */
-
 import React, { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 import "../css/Login.css";
-import axios from "axios"
+
 
 const Login = () => {
   const navigate = useNavigate();
@@ -18,31 +17,33 @@ const Login = () => {
   }, [navigate]);
 
   const handleSubmit = async (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  try {
-    const res = await axios.post("https://schoolvan-1.onrender.com/api/auth/login", {
-      email,
-      password
-    });
+    try {
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
 
-    console.log("Response:", res);
+      console.log("resposse printed",res)
 
-    const data = res.data;
-    console.log(data);
+      const data = await res.json();
+      console.log(data)
 
-    if (data.ok) {
-      localStorage.setItem("isLoggedIn", "true");
-      Swal.fire("Login Successful!", "", "success");
-      navigate("/home");
-    } else {
-      Swal.fire("Invalid Credentials", data.error || "Please check email or password", "error");
+      if (data.ok) {
+        // Login successful
+        localStorage.setItem("isLoggedIn", "true");
+        Swal.fire("Login Successful!", "", "success");
+        navigate("/home");
+      } else {
+        // Login failed
+        Swal.fire("Invalid Credentials", data.error || "Please check email or password", "error");
+      }
+    } catch (error) {
+      Swal.fire("Error", "Something went wrong. Try again later.", "error");
     }
-  } catch (error) {
-    console.error("Login error:", error);
-    Swal.fire("Error", "Something went wrong. Try again later.", "error");
-  }
-};
+  };
 
   return (
     <div className="login-wrapper">
