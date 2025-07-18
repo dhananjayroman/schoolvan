96c8ef6a6b451a86f435685e683e53bcdfb69751
Dhananjay Roman <romandhananjay01@gmail.com>
2025-07-18 12:01:26 +0530

    new two latest Updated backend json file and app.js files

--- Backend/app.js
+++ Backend/app.js
@@ -78,11 +78,7 @@ app.get("/", (req, res) => {
 });
 
 
-app.use(express.static(path.join(__dirname, "./Frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, ".Frontend/build/index.html"));
-});
 
 
 
